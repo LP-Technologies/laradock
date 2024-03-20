6d0a5ffce1ad9a28ed1d07d52269ce01f5b5406b
Kirill Latish <kirill.latish@gmail.com>
2024-03-20 08:48:28 +0200

    Added new certificate check

--- php-fpm/download-certificate.sh
+++ php-fpm/download-certificate.sh
@@ -16,3 +16,20 @@ else
     echo "File has not changed."
     rm "${TEMP_FILE}"
 fi
+
+CA_URL="https://iit.com.ua/download/productfiles/CAs.json"
+CA_DEST_DIR="/var/certificates"
+CA_DEST_FILE="${CA_DEST_DIR}/CAs.json"
+CA_TEMP_FILE="${CA_DEST_DIR}/CAs.temp.json"
+
+# Download the file to a temporary location
+curl -o "${CA_TEMP_FILE}" "${CA_URL}"
+
+# Check if the file has changed
+if ! cmp -s "${CA_TEMP_FILE}" "${CA_DEST_FILE}"; then
+    echo "File has changed, updating..."
+    mv "${CA_TEMP_FILE}" "${CA_DEST_FILE}"
+else
+    echo "File has not changed."
+    rm "${CA_TEMP_FILE}"
+fi
